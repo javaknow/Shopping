198815c322286a6d4500d7b8acf3397b6d383b8a
shewenbiao <shewenbiao@gmail.com>
2015-04-25 19:20:21 +0800

    create navigation drawer

---
+++ app/src/main/java/com/shopping/swb/shopping/activity/MainActivity.java
@@ -0,0 +1,89 @@
+package com.shopping.swb.shopping.activity;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.shopping.swb.shopping.R;
+
+
+public class MainActivity extends ActionBarActivity implements DrawerLayout.DrawerListener{
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setNavDrawer();
+    }
+
+    private void setNavDrawer(){
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerSlide(View view, float v) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(View view) {
+
+    }
+
+    @Override
+    public void onDrawerClosed(View view) {
+
+    }
+
+    @Override
+    public void onDrawerStateChanged(int i) {
+
+    }
+}
---
+++ app/src/main/java/com/shopping/swb/shopping/application/ShoppingApplicaiton.java
@@ -0,0 +1,16 @@
+package com.shopping.swb.shopping.application;
+
+import android.app.Application;
+
+/**
+ * Description:
+ * Author: SheWenBiao
+ * Date: 2015-04-25
+ * Time: 16:33
+ */
+public class ShoppingApplicaiton extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
