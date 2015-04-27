48c5fa7f25c089cc55def3d24a44f4c3463fcbaa
shewenbiao <shewenbiao@gmail.com>
2015-04-28 02:09:32 +0800

    Add some layout and button
    1.Add layout to navigation drawer
    2.Add search,share,settings button in toolbar

--- app/src/main/java/com/shopping/swb/shopping/activity/MainActivity.java
+++ app/src/main/java/com/shopping/swb/shopping/activity/MainActivity.java
@@ -1,38 +1,66 @@
 package com.shopping.swb.shopping.activity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.shopping.swb.shopping.R;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
-public class MainActivity extends ActionBarActivity implements DrawerLayout.DrawerListener{
+
+public class MainActivity extends ActionBarActivity implements DrawerLayout.DrawerListener
+            ,View.OnClickListener{
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private View mCustomTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setNavDrawer();
+        setCustomTitle();
+        setOverflowShowingAlways();
     }
 
     private void setNavDrawer(){
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
         setSupportActionBar(mToolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
                 R.string.drawer_close);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
+        if(!mDrawerLayout.isDrawerOpen(Gravity.START)){
+            mDrawerLayout.openDrawer(Gravity.START);
+        }
     }
 
+    private  void setCustomTitle(){
+        mCustomTitle = LayoutInflater.from(this).inflate(R.layout.actionbar_custom_title_layout,null);
+        ((TextView)mCustomTitle).setText(R.string.search_all);
+        mCustomTitle.setOnClickListener(this);
+        ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(Gravity.CENTER);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(mCustomTitle, layoutParams);
+    }
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
@@ -44,7 +72,16 @@ public class MainActivity extends ActionBarActivity implements DrawerLayout.Draw
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
-
+    private void setOverflowShowingAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            menuKeyField.setAccessible(true);
+            menuKeyField.setBoolean(config, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -60,11 +97,35 @@ public class MainActivity extends ActionBarActivity implements DrawerLayout.Draw
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Intent intent = null;
+        switch (id){
+            case R.id.action_search:
+                 intent = new Intent(this,SearchActivity.class);
+                break;
+            case R.id.action_share:
+                break;
+            case R.id.action_settings:
+                break;
         }
-
+        if(intent != null) {
+            startActivity(intent);
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
     }
 
     @Override
@@ -85,5 +146,30 @@ public class MainActivity extends ActionBarActivity implements DrawerLayout.Draw
     @Override
     public void onDrawerStateChanged(int i) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
