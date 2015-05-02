150e6c87d157832b682ed1afae5c636316231954
shewenbiao <shewenbiao@gmail.com>
2015-05-02 23:01:46 +0800

    Add animation for delete single search history

--- app/src/main/java/com/shopping/swb/shopping/adapter/SearchHistoryAdapter.java
+++ app/src/main/java/com/shopping/swb/shopping/adapter/SearchHistoryAdapter.java
@@ -1,8 +1,12 @@
 package com.shopping.swb.shopping.adapter;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -26,7 +30,7 @@ public class SearchHistoryAdapter extends DataBaseAdapter<String>{
     }
     @Override
     public View createView(final int position, View convertView, ViewGroup parent) {
-        ViewHolder viewHolder;
+        final ViewHolder viewHolder;
         if(convertView == null){
             convertView = mInflater.inflate(R.layout.item_search_list,parent,false);
             viewHolder = new ViewHolder();
@@ -37,14 +41,13 @@ public class SearchHistoryAdapter extends DataBaseAdapter<String>{
             viewHolder = (ViewHolder) convertView.getTag();
         }
         viewHolder.textView.setText(mList.get(position));
+        final View finalConvertView = convertView;
         viewHolder.delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mList.remove(position);
-                notifyDataSetChanged();
-                if(getCount() == 0) {
-                    mCallback.handleNoData();
-                }
+                Animation animation = AnimationUtils.loadAnimation(mContext,R.anim.anim_delete);
+                finalConvertView.startAnimation(animation);
+                mHandler.sendEmptyMessageDelayed(position,300);
             }
         });
         return convertView;
@@ -56,4 +59,16 @@ public class SearchHistoryAdapter extends DataBaseAdapter<String>{
     public void setNoDataCallback(NoDataCallback callback){
         this.mCallback = callback;
     }
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            mList.remove(msg.what);
+            notifyDataSetChanged();
+            if(getCount() == 0) {
+                mCallback.handleNoData();
+            }
+        }
+    };
+
 }
