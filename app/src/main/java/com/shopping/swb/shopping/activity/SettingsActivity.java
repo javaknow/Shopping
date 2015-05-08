510080001029aa37ef547d70c1555548f31b1b73
shewenbiao <shewenbiao@gmail.com>
2015-05-08 11:39:39 +0800

    Add clear cache

--- app/src/main/java/com/shopping/swb/shopping/activity/SettingsActivity.java
+++ app/src/main/java/com/shopping/swb/shopping/activity/SettingsActivity.java
@@ -1,13 +1,18 @@
 package com.shopping.swb.shopping.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.shopping.swb.shopping.R;
+import com.shopping.swb.shopping.constant.AppConstants;
 import com.shopping.swb.shopping.util.Utility;
 import com.umeng.fb.FeedbackAgent;
 import com.umeng.fb.fragment.FeedbackFragment;
@@ -16,7 +21,7 @@ import com.umeng.message.UmengRegistrar;
 
 public class SettingsActivity extends BaseActivity implements View.OnClickListener{
     private Toolbar mToolbar;
-    private View mFeedback,mAbout;
+    private View mFeedback,mCheckUpdate,mClearCache,mAbout;
     private FeedbackAgent mFeedbackAgent;
     private PushAgent mPushAgent;
 
@@ -42,10 +47,14 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
                 finish();
             }
         });
-        mFeedback = findViewById(R.id.feedback);
-        mFeedback.setOnClickListener(this);
+        mCheckUpdate = findViewById(R.id.check_update);
+        mCheckUpdate.setOnClickListener(this);
+        mClearCache = findViewById(R.id.clear_cache);
+        mClearCache.setOnClickListener(this);
         mAbout = findViewById(R.id.about);
         mAbout.setOnClickListener(this);
+        mFeedback = findViewById(R.id.feedback);
+        mFeedback.setOnClickListener(this);
     }
 
     @Override
@@ -74,16 +83,41 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
     public void onClick(View v) {
         int id = v.getId();
         switch (id){
-            case R.id.feedback:
-            //    mFeedbackAgent.startFeedbackActivity();
-                Intent intent = new Intent(this,FeedbackActivity.class);
-                String feedbackId = mFeedbackAgent.getDefaultConversation().getId();
-                intent.putExtra(FeedbackFragment.BUNDLE_KEY_CONVERSATION_ID,feedbackId);
-                startActivity(intent);
+            case R.id.check_update:
+                Toast.makeText(this,R.string.check_update_hint,Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.clear_cache:
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.clear_cache_dialog_title)
+                        .setMessage(R.string.clear_cache_dialog_message)
+                        .setPositiveButton(R.string.btn_ok,new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this)
+                                        .edit().putString(AppConstants.HISTORIES_LIST,"").apply();
+                                dialog.cancel();
+                                Toast.makeText(SettingsActivity.this,R.string.clear_cache_success_hint,Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(R.string.btn_cancel,new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        })
+                        .create()
+                        .show();
                 break;
             case R.id.about:
                 Intent aboutIntent = new Intent(this,AboutActivity.class);
                 startActivity(aboutIntent);
+                break;
+            case R.id.feedback:
+                //    mFeedbackAgent.startFeedbackActivity();
+                Intent intent = new Intent(this,FeedbackActivity.class);
+                String feedbackId = mFeedbackAgent.getDefaultConversation().getId();
+                intent.putExtra(FeedbackFragment.BUNDLE_KEY_CONVERSATION_ID,feedbackId);
+                startActivity(intent);
                 break;
         }
     }
